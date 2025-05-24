54fb9c1c966be46be897b79bf8955e8b18f59c86
pedroabueno06 <pedro.ascenco@gmail.com>
2025-05-23 21:27:45 -0300

    feat: model Projeto

---
+++ src/main/java/models/Projeto.java
@@ -0,0 +1,24 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "projeto")
+public class Projeto {
+    @Id
+    private int id;
+    private String descricao;
+    private LocalDate dtaaInicio;
+    private LocalDate dataPrevistaFim;
+
+    public Projeto(int id, String descricao, LocalDate dtaaInicio, LocalDate dataPrevistaFim) {
+        this.id = id;
+        this.descricao = descricao;
+        this.dtaaInicio = dtaaInicio;
+        this.dataPrevistaFim = dataPrevistaFim;
+    }
+}
